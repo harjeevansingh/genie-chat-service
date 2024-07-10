9efd2e2a1cedd3cd4600bc2ec8ee7d22022b46c5
harjeevansingh <harjeevansingh01@gmail.com>
2024-07-11 05:29:45 +0530

    fix: time format

--- src/main/java/com/chatservice/dto/MessageDTO.java
+++ src/main/java/com/chatservice/dto/MessageDTO.java
@@ -2,6 +2,7 @@ package com.chatservice.dto;
 
 import lombok.Data;
 import java.time.LocalDateTime;
+import com.fasterxml.jackson.annotation.JsonFormat;
 
 /**
  * Author: harjeevansingh
@@ -13,6 +14,7 @@ public class MessageDTO {
     private Long conversationId;
     private SenderType senderType;
     private String content;
+    @JsonFormat(pattern = "yyyy-MM-dd'T'HH:mm:ss.SSS'Z'")
     private LocalDateTime timestamp = LocalDateTime.now();
 
     public enum SenderType {
